e316a85c03d9d7dc02716432e966f309be205317
Suraj Jagtap <suraj552003@gmail.com>
2022-11-06 13:13:17 +0530

    First Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b9c56a43-868e-41e4-89c5-fac1e3ffb93b';
+export const MODAL_CLOSE_SEC = 2.5;
